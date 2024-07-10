e213235493d6775a053d7f95320cb024068a0656
thanh <thanh3003work@gmail.com>
2024-07-10 16:50:55 +0700

    Idle & Wander using NavMeshAgent

--- Assets/Game Folder/Scripts/Entity/Enemy/State Manager/Actions/ActionWander.cs
+++ Assets/Game Folder/Scripts/Entity/Enemy/State Manager/Actions/ActionWander.cs
@@ -1,9 +1,55 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ActionWander : FSM_Action
 {
+    // --------------------------------- Attribute
+    [Header("Animation")]
+    private EnemyAnimation enemyAnimation;
+    // ---------------------------------
+    [Header("NavMeshAgent Component")]
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private LayerMask groundMask;
+    // ---------------------------------
+    [Header("Walk Point Elements")]
+    private Vector3 walkPoint;
+    private bool walkPointSet;
+    [SerializeField] private float walkPointRange;
+    
+    // --------------------------------- Unity Functions
+
+    void Awake()
+    {
+        enemyAnimation = GetComponentInChildren<EnemyAnimation>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // --------------------------------- User Defined Functions
+    
     public override void Act()
     {
-        throw new System.NotImplementedException();
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet) {
+            enemyAnimation.SetIdleAnimation(false);
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;    
+        // walkPoint reached
+        if (distanceToWalkPoint.magnitude < 1f) {
+            walkPointSet = false;
+        }   
+    }
+
+    public void SearchWalkPoint() {
+        // Calculate random point in range
+        float randomX = Random.Range(- walkPointRange, walkPointRange);
+        float randomZ = Random.Range(- walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if (Physics.Raycast(walkPoint, - transform.up, 2f, groundMask)) {
+            walkPointSet = true;
+        }
     }
 }
